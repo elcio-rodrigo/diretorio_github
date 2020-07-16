f62898a4845d2ae59ccd8df995b0fe60e455c379
Elcio Rodrigo <e.rhodrigo.a@gmail.com>
2020-07-16 15:11:40 -0300

    Resolução do exercício 3

---
+++ Trybe/exercises/4_4/exercicio3.js
@@ -0,0 +1,9 @@
+let info = {
+    personagem: "Margarida",
+    origem: "pato Donald",
+    nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
+    recorrente: "Sim"
+}
+for (let chave in info) {
+    console.log(chave);
+}
