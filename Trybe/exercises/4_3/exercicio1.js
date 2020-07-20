e006ea461ce3986285c390581f5635ed9bef40e1
Elcio Rodrigo <e.rhodrigo.a@gmail.com>
2020-07-20 17:06:28 -0300

    edit

--- Trybe/exercises/4_3/exercicio1.js
+++ Trybe/exercises/4_3/exercicio1.js
@@ -3,9 +3,9 @@ let n = 5;
 let asterisco = " * ";
 let linha = "";
 
-for (let i = 0; i < n; i++) {
+for (let i = 0; i < n; i += 1) {
     linha += asterisco;
 }
-for (let i = 0; i < n; i++) {
+for (let i = 0; i < n; i += 1) {
     console.log(linha);
 }
