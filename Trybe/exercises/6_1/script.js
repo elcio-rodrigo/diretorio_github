52bbe669b01e586a2cf780418c1f5bb0ac1e224d
Elcio Rodrigo <e.rhodrigo.a@gmail.com>
2020-08-03 14:21:45 -0300

    6_1

---
+++ Trybe/exercises/6_1/script.js
@@ -0,0 +1,10 @@
+let state = document.getElementById('estados');
+let uf = ['Acre', 'Alagoas', 'Amapá', 'Amazonas', 'Bahia', 'Ceará', 'Distrito Federal', 'Espírito Santo', 'Goiás', 'Maranhão', 'Mato Grosso', 'Mato Grosso do Sul', 'Minas Gerais', 'Pará', 'Paraíba', 'Paraná', 'Pernambuco', 'Piauí', 'Rio de Janeiro', 'Rio Grande do Norte', 'Rio Grande do Sul', 'Rondônia', 'Roraima', 'Santa Catarina', 'São Paulo', 'Sergipe', 'Tocantins'];
+let siglas = ['AC', 'AL', 'AP', 'AM', 'BH', 'CE', 'DF', 'ES', 'GO', 'MA', 'MT', 'MS', 'MG', 'PA', 'PB', 'PR', 'PE', 'PI', 'RJ', 'RN', 'RS', 'RO', 'RR', 'SC', 'SP', 'SE', 'TO']
+for (let i = 0; i < uf.length; i++) {
+    let estado = document.createElement('option');
+    estado.value = siglas[i];
+    estado.innerText = uf[i];
+    state.appendChild(estado);
+}
+
