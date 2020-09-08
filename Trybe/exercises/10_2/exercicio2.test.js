c0e783d59b2e0074a427670b673b4671bb07957e
Elcio Rodrigo <e.rhodrigo.a@gmail.com>
2020-09-08 11:38:02 -0300

    Bloco 10 dia 2 exercicios

---
+++ Trybe/exercises/10_2/exercicio2.test.js
@@ -0,0 +1,37 @@
+const users = {
+    4: { name: 'Mark' },
+    5: { name: 'Paul' }
+    };
+    
+    const findUserById = (id) => {
+      return new Promise((resolve, reject) => {
+          if (users[id]) {
+            return resolve(users[id]);
+          }
+    
+          return reject({ error: 'User with ' + id + ' not found.' });
+      });
+    }
+    
+    const getUserName = (userID) => {
+      return findUserById(userID).then(user => user.name);
+    }
+// Utilizando a sintaxe de Promise, faça um teste que verifique o resultado da função getUserName para o caso em que o usuário é encontrado, e também um teste para o caso em que o usuário não é encontrado.
+// Dica: Veja os dados falsos utilizados no banco de dados, disponíveis na variável users, para saber quais IDs existem.
+describe('getUserName - promise', () => {
+    describe('when the user id exists', () => {
+      it('returns the user name', () => {
+        expect.assertions(1);
+        return getUserName(4).then(data => expect(data).toEqual('Mark'));
+      });
+    });
+  
+    describe('when the user id does not exists', () => {
+      it('returns an error', () => {
+        expect.assertions(1);
+        return getUserName(2).catch(error =>
+          expect(error).toEqual({ error: 'User with 2 not found.' })
+        );
+      });
+    });
+  });
---
+++ Trybe/exercises/10_2/exercicio4.test.js
@@ -0,0 +1,19 @@
+// O código abaixo busca, no GitHub de um usuário, de acordo com a URL, seus repositórios e retorna uma lista como resultado. Dada a URL 'https://api.github.com/users/tryber/repos', faça um teste que verifique que os repositórios 'sd-01-week4-5-project-todo-list' e 'sd-01-week4-5-project-meme-generator' se encontram nessa lista.
+const fetch = require('node-fetch');
+
+const getRepos = (url) => {
+  return fetch(url)
+    .then(response => response.json())
+    .then((data) => {
+      return data.map((repo) => repo.name)
+    });
+}
+
+it('gets a list of repositories names', () => {
+  const url = 'https://api.github.com/users/tryber/repos';
+
+  return getRepos(url).then(result => {
+    expect(result).toContain('sd-01-week4-5-project-todo-list');
+    expect(result).toContain('sd-01-week4-5-project-meme-generator');
+  });
+});
